ced04fc58565edb90aa94b1b7c06c68ab376b4ae
lorondo <mralexanderlindsay@gmail.com>
2025-06-06 01:15:42 -0700

    Updated ALLOWED_HOSTS with site and development server URL

--- recipeapp/settings.py
+++ recipeapp/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['shielded-gorge-98258-d938c144ba8f.herokuapp.com']
 
 
 # Application definition
