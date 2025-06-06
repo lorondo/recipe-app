073a817d4473211bebcb832e48e1289cbcc351ed
lorondo <mralexanderlindsay@gmail.com>
2025-06-06 13:39:12 -0700

    Trying gunicorn setup for Heroku

--- recipeapp/wsgi.py
+++ recipeapp/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+print("Starting WSGI...")
+print("Python path:", sys.path)
+print("DJANGO_SETTINGS_MODULE:", os.environ.get('DJANGO_SETTINGS_MODULE'))
 
 from django.core.wsgi import get_wsgi_application
 
